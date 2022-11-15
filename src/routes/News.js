c58e449fd1a5a6d3734c847fc898ee557ac71c8f
Daeil Choi <daeil.choi@classact.co.kr>
2022-11-15 16:58:48 +0900

    fix: 광고 재노출:

--- src/routes/News.js
+++ src/routes/News.js
@@ -87,7 +87,7 @@ const News = ({ userObj }) => {
   }
 
   useEffect(() => {
-    // adfitLong();
+    adfitLong();
     adfitLong2();
     adfitMobile();
     adfitMobile2();
@@ -105,7 +105,7 @@ const News = ({ userObj }) => {
           로그인 후 등록 가능합니다. 단, 부적절한 이용은 제재를 받을 수
           있습니다.
         </span>
-        {/* <div className="adfit adfit-l"></div> */}
+        <div className="adfit adfit-l"></div>
         <div className="adfit adfit-m2"></div>
         <div className="news__type">
           <label
