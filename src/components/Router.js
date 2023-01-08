65a2af18db3b7d563d5b5b445cdd0b6244511c81
clowncdi <clowncdi85@gmail.com>
2023-01-08 23:42:30 +0900

    feat: reack router code splitting

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,56 +1,60 @@
+import React, { Suspense, lazy } from 'react';
 import { Navigate, Route, Routes } from "react-router-dom";
-import Auth from "routes/Auth";
-import Home from "routes/Home";
-import Write from "routes/Write";
-import NewsWrite from "routes/NewsWrite";
-import Navigation from "./Navigation";
-import News from "routes/News";
-import Infomation from "routes/Infomation";
-import WeacoDetail from "routes/WeacoDetail";
-import NewsDetail from "routes/NewsDetail";
-import ImageCompressior from "routes/ImageCompressior";
+
+const Auth = lazy(() => import("routes/Auth"));
+const Home = lazy(() => import("routes/Home"));
+const Write = lazy(() => import("routes/Write"));
+const NewsWrite = lazy(() => import("routes/NewsWrite"));
+const Navigation = lazy(() => import("./Navigation"));
+const News = lazy(() => import("routes/News"));
+const Infomation = lazy(() => import("routes/Infomation"));
+const WeacoDetail = lazy(() => import("routes/WeacoDetail"));
+const NewsDetail = lazy(() => import("routes/NewsDetail"));
+const ImageCompressior = lazy(() => import("routes/ImageCompressior"));
 
 function AppRouter({ isLoggedIn, userObj }) {
   return (
     <>
-      {isLoggedIn ? (
-        <Navigation userObj={userObj} isLoggedIn={isLoggedIn} />
-      ) : (
-        <Navigation isLoggedIn={isLoggedIn} />
-      )}
-      <Routes>
-        <Route exact path="/" element={<Home userObj={userObj} />}></Route>
-        <Route path="/:id" element={<WeacoDetail userObj={userObj} />}></Route>
-        <Route path="/news" element={<News userObj={userObj} />}></Route>
-        <Route
-          path="/news/:id"
-          element={<NewsDetail userObj={userObj} />}
-        ></Route>
-        <Route path="/login" element={<Auth isLoggedIn={isLoggedIn} />}></Route>
-        <Route path="/info" element={<Infomation />}></Route>
-        <Route path="*" element={<Navigate replace to={"/"} />}></Route>
-        {isLoggedIn && (
-          <>
-            <Route path="/write" element={<Write userObj={userObj} />}></Route>
-            <Route
-              path="/write/:id"
-              element={<Write userObj={userObj} />}
-            ></Route>
-            <Route
-              path="/news/write"
-              element={<NewsWrite userObj={userObj} />}
-            ></Route>
-            <Route
-              path="/news/write/:id"
-              element={<NewsWrite userObj={userObj} />}
-            ></Route>
-            <Route
-              path="/image-comp"
-              element={<ImageCompressior />}
-            ></Route>
-          </>
+      <Suspense fallback={<div className="loading"></div>}>
+        {isLoggedIn ? (
+          <Navigation userObj={userObj} isLoggedIn={isLoggedIn} />
+        ) : (
+          <Navigation isLoggedIn={isLoggedIn} />
         )}
-      </Routes>
+        <Routes>
+          <Route exact path="/" element={<Home userObj={userObj} />}></Route>
+          <Route path="/:id" element={<WeacoDetail userObj={userObj} />}></Route>
+          <Route path="/news" element={<News userObj={userObj} />}></Route>
+          <Route
+            path="/news/:id"
+            element={<NewsDetail userObj={userObj} />}
+          ></Route>
+          <Route path="/login" element={<Auth isLoggedIn={isLoggedIn} />}></Route>
+          <Route path="/info" element={<Infomation />}></Route>
+          <Route path="*" element={<Navigate replace to={"/"} />}></Route>
+          {isLoggedIn && (
+            <>
+              <Route path="/write" element={<Write userObj={userObj} />}></Route>
+              <Route
+                path="/write/:id"
+                element={<Write userObj={userObj} />}
+              ></Route>
+              <Route
+                path="/news/write"
+                element={<NewsWrite userObj={userObj} />}
+              ></Route>
+              <Route
+                path="/news/write/:id"
+                element={<NewsWrite userObj={userObj} />}
+              ></Route>
+              <Route
+                path="/image-comp"
+                element={<ImageCompressior />}
+              ></Route>
+            </>
+          )}
+        </Routes>
+      </Suspense>
     </>
   );
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -226,7 +226,6 @@ const Home = ({ userObj }) => {
   }
 
   useEffect(() => {
-    // adfitLong();
     adfitLong2();
     adfitMobile();
     adfitMobile2();
